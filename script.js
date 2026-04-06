cd4ddf767365d85040c6fb2c49b52e46a4daa54e
Emily-Mae <ermcconihe@ucdavis.edu>
2026-04-06 13:25:23 -0700

    homeisfirt

--- script.js
+++ script.js
@@ -3,27 +3,41 @@
 
     const button = document.querySelector('button');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
-    let mode = 'dark';
+    const container = document.querySelector('#container');
+    const header = document.querySelector('header');
+    const h1 = document.querySelector('h1');
+    const h2 = document.querySelector('h2');
+    const tiles = document.querySelector('.tiles');
+    let mode = 'outside';
 
     button.addEventListener('click', function() {
-        if (mode === 'dark') {
+        if (mode === 'outside') {
+            container.className = 'switch';
             body.className = 'switch';
-            banner.className = 'switch';
+            header.className = 'switch';
+            h1.className = 'switch';
+            h2.className = 'switch';
             button.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
-            mode = 'light';
+            mode = 'inside';
         } else {
+            container.removeAttribute('class');
             body.removeAttribute('class');
-            banner.removeAttribute('class');
+            header.removeAttribute('class');
+            h1.removeAttribute('class');
+            h2.removeAttribute('class');
             button.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
-            mode = 'dark'
+            // for (const tile of tiles) {
+            //     tile.className = 'switch';
+            //     // section.className = '.tiles';
+            // }
+            mode = 'outside'
         }
     })
 })();
