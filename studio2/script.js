cdc7bc023e615f50eec923eb9af000859a643640
Emily-Mae <ermcconihe@ucdavis.edu>
2026-05-08 01:46:02 -0700

    check yourself final1

--- studio2/script.js
+++ studio2/script.js
@@ -9,92 +9,52 @@
     const myTemp = document.querySelector('#my-temp');
     const myWeather = document.querySelector('#my-weather');
     const myMood = document.querySelector('#my-mood');
+
     const tiles = document.querySelectorAll('.tiles');
+    const tileTime = document.querySelectorAll('.tiles p');
+    const info = document.querySelector('#info');
+    const des = document.querySelector('#description');
 
-    const point1 = document.querySelector('#point1');
+    for(const tile of tiles){
+        // tile.addEventListener('click', function(event){
+        //     info.className = 'fade';
+        // });
 
-    //i want the data to populate on click, does the thing for acutally populating the data need to be a separate function - i also don't really understand how to get the data beyond the super basic functions
-    //i don't get the global data thing at all 
+        // tile.addEventListener('mouseover', function(event){
+        //     tileTime.className = '.grow';
+        // })
 
-    // let globalData;
+    }
 
     async function getData(){
         const fetchPromise = await fetch('data/data.json');
         const data = await fetchPromise.json();
 
-        // const keys = Object.keys(data);
-        // globalData = Object.keys(data);
-        
         console.log(data);
         populate(data);
-
-        // document.querySelector('#img-container img').src = data.img2.img;
     };
 
     function populate(data){
 
-        // const dataPoints = Object.keys(data);
-        // console.log(dataPoints);
-        // dataPoints.forEach( function(eachPoint){
-    
-        // });
+        for(const tile of tiles){
+            tile.addEventListener('click', function (event) {
+                event.preventDefault();
 
-        point1.addEventListener('click', function(){
-            // myImg.src = data.point1.img;
-            myTime.innerHTML = data.point1.time;
-            myDate.innerHTML = `date: ${data.point1.date}`;
-            myTemp.innerHTML = `temperature: ${data.point1.temperature}`;
-            myWeather.innerHTML = `weather: ${data.point1.weather}`;
-            myMood.innerHTML = `mood: ${data.point1.mood}`;
-        });
+                // console.log(tile.id);
 
+                myImg.src = data[tile.id].img;
+                myImg.style.visibility = 'visible';
+                myTime.innerHTML = `time: ${data[tile.id].time}`;
+                myDate.innerHTML = `date: ${data[tile.id].date}`;
+                myTemp.innerHTML = `temperature: ${data[tile.id].temperature}`;
+                myWeather.innerHTML = `weather: ${data[tile.id].weather}`;
+                myMood.innerHTML = `mood: ${data[tile.id].mood}`;
+                des.style.visibility = 'hidden';
+
+            });
+        }
+ 
     }
-
-        // for( const tile of tiles){
-        //     tile.addEventListener('click', function(){
-
-
-        //         // keys.forEach( function(eachPoint){
-        //         //     myTime.innerHTML = data.point2.time;
-        //         // })
-
-        //         // myImg.src = data.point.img;
-        //         myTime.innerHTML = data.point.time;
-        //         myDate.innerHTML = data.point.date;
-        //         myTemp.innerHTML = data.point.temperature;
-        //         myWeather.innerHTML = data.point.weather;
-        //         myMood.innerHTML = data.point.mood;
-
-        //         console.log('return');
-        //     });
-        // }
-
-        
-    
-
-        // const dataPoints = Object.values(data);
-        // dataPoints.forEach( function(point){
-
-        //     for( const tile of tiles){
-        //         tile.addEventListener('click', function(){
-
-        //             // myImg.src = data.point.img;
-        //             myTime.innerHTML = `${data[point].time}`;
-        //             myDate.innerHTML = data.point.date;
-        //             myTemp.innerHTML = data.point.temperature;
-        //             myWeather.innerHTML = data.point.weather;
-        //             myMood.innerHTML = data.point.mood;
-
-        //             console.log('return');
-        //         });
-        //     }
-
-        // });
-
-
-
-
-    // document.querySelector('.tiles').addEventListener('click', )
 
     getData();
 
