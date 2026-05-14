fd20086a0cedac809b41d4bc1861288a6586c709
Emily-Mae <ermcconihe@ucdavis.edu>
2026-05-13 22:54:14 -0700

    library liberty

--- studio3/script.js
+++ studio3/script.js
@@ -2,6 +2,15 @@
     'use strict';
     console.log('reading js');
 
-    
+    AOS.init();
+
+    new TypeIt("#typing-test", {
+        strings: "Kenjockety",
+        speed: 150,
+        waitUntilVisible: true,
+        afterComplete: function (instance) {
+            instance.destroy();
+        }
+    }).go();
 
 })();
