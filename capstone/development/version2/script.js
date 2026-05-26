e6d18d014a6a308cb1af1d3bdc2018ae2a6209bc
Emily-Mae <ermcconihe@ucdavis.edu>
2026-05-26 15:50:06 -0700

    v2 update2

--- capstone/development/version2/script.js
+++ capstone/development/version2/script.js
@@ -4,10 +4,18 @@
 
     const house = document.querySelector('#house');
     const houseOverlay = document.querySelector('#house-overlay');
+    const backArrow = document.querySelector('.fa-solid');
 
     house.addEventListener('click', function(event){
         console.log('click');
         houseOverlay.className = "overlay-onscreen";
     });
+
+    backArrow.addEventListener('click', function(){
+        console.log('click');
+        houseOverlay.className = "overlay-offscreen";
+    });
+
+
 
 })();
